bf4fd9f8c59d3a56c5289ade918540280dd0a390
David <learnitmyway@users.noreply.github.com>
2020-05-29 16:14:54 +0200

    Implement fizzBuzz

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,1 +1,17 @@
-export default function fizzBuzz() {}
+function processEntry(entry) {
+  const multipleOf3 = entry % 3 === 0
+  const multipleOf5 = entry % 5 === 0
+  if (multipleOf3 && multipleOf5) {
+    return 'FizzBuzz'
+  } else if (multipleOf3) {
+    return 'Fizz'
+  } else if (multipleOf5) {
+    return 'Buzz'
+  } else {
+    return entry
+  }
+}
+
+export default function fizzBuzz(input) {
+  return input.map(processEntry).join(', ')
+}
--- src/fizzBuzz.test.js
+++ src/fizzBuzz.test.js
@@ -2,6 +2,15 @@ import fizzBuzz from './fizzBuzz'
 
 describe('fizzBuzz', () => {
   it('executes', () => {
-    expect(fizzBuzz()).toBe(undefined)
+    expect(fizzBuzz([1])).toBe('1')
+    expect(fizzBuzz([1, 2])).toBe('1, 2')
+    expect(fizzBuzz([1, 2, 3])).toBe('1, 2, Fizz')
+    expect(fizzBuzz([1, 2, 3, 5])).toBe('1, 2, Fizz, Buzz')
+    expect(fizzBuzz([1, 2, 3, 5, 6, 10])).toBe(
+      '1, 2, Fizz, Buzz, Fizz, Buzz'
+    )
+    expect(fizzBuzz([1, 2, 3, 5, 6, 10, 15])).toBe(
+      '1, 2, Fizz, Buzz, Fizz, Buzz, FizzBuzz'
+    )
   })
 })
