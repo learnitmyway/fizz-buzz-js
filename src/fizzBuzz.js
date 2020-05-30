25e805321b90720f63a59b277839de2927068974
David <learnitmyway@users.noreply.github.com>
2020-05-30 16:38:31 +0200

    Only use if statements

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,15 +1,20 @@
 function processEntry(entry) {
   const multipleOf3 = entry % 3 === 0
   const multipleOf5 = entry % 5 === 0
+
   if (multipleOf3 && multipleOf5) {
     return 'FizzBuzz'
-  } else if (multipleOf3) {
-    return 'Fizz'
-  } else if (multipleOf5) {
-    return 'Buzz'
-  } else {
-    return entry
   }
+
+  if (multipleOf3) {
+    return 'Fizz'
+  }
+
+  if (multipleOf5) {
+    return 'Buzz'
+  }
+
+  return entry
 }
 
 export default function fizzBuzz(input) {
